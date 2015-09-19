e82d09e670ef31894b11bdba626a2ae2b502f25d
Geoff Irvine <mail@geoffirvine.com>
2015-09-19 22:20:18 +1000

    Header and nav styles

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,25 @@
 
-<header class="banner" role="banner" data-spy="affix">
+<header class="banner" role="banner" data-spy="affix" data-offset-top="100">
   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".pg-nav-collapse" aria-expanded="false">
-        <span class="">Toggle navigation</span>
+        <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+
+      
   
     
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>"><span class="sr-only"><?php bloginfo('name'); ?></span></a>
+    
 
-    <nav class="secondary-navigation" role="navigation" aria-label="Secondary Navigation">
+    
+
+
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-2 col-sm-4">
+          <nav class="secondary-navigation" role="navigation" aria-label="Secondary Navigation">
       <div class="menu-top-menu-container">
         <ul id="menu-top-menu" class="menu">
           <li><a href="">FAQ</a></li>
@@ -20,28 +29,25 @@
         </ul>
       </div>
     </nav>
-
-<ul class="site-header-cart menu">
+        </div>
+        <div class="col-xs-8 col-sm-4 text-center">
+          <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>"><span class="sr-only"><?php bloginfo('name'); ?></span></a>
+        </div>
+        <div class="col-xs-2 col-sm-4">
+          <div class="cart-toggle"><i class="fa fa-shopping-cart"></i></div>
+          <ul class="site-header-cart menu">
       <li class="">
+        
         <a class="cart-contents" href="http://dev-bc.pantheon.io/cart/" title="View your shopping cart">
         <i class="fa fa-shopping-cart"></i> $0.00 <span class="count">0 items</span>
       </a>
     
           </li>
-      <li>
-        <div class="widget woocommerce widget_shopping_cart"><div class="widget_shopping_cart_content">
-
-<ul class="cart_list product_list_widget ">
-
-  
-    <li class="empty">No products in the cart.</li>
-
-  
-</ul><!-- end product list -->
-
-
-</div></div>      </li>
+      
     </ul>
+        </div>
+      </div>
+    </div>
     
     <nav role="navigation">
       <?php
