850a78c54464ae722b51b4c6df25034666343760
Alex James Bishop <alex@internetmonkey.com.au>
2015-09-20 23:29:12 +0930

    remove typekit because they break the internet

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,4 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <script src="https://use.typekit.net/avx7alx.js"></script>
-  <script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
