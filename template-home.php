01fffb35ef37994c88cca52a6f41f5bc4466719e
Alex James Bishop <alex@internetmonkey.com.au>
2016-03-24 15:20:43 +1030

    fix animation/wow flickering issues on page load

--- template-home.php
+++ template-home.php
@@ -13,7 +13,7 @@
   	
   		<div class="table-box">
   			
-  				<div class="table-cell-box wow fadeIn">
+  				<div class="table-cell-box wow fadeIn" style="visibility:hidden;" >
   					<div class="hero-text">
   						<div class="container">
   							<div class="row">
@@ -39,7 +39,7 @@
   			
   		
   	</div>
-  	<div class="hero-image visible-md visible-lg wow fadeInRight">
+  	<div class="hero-image visible-md visible-lg wow fadeInRight" style="visibility:hidden;" >
   		
   				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/dist/images/balsa_sheets.png" alt="">
 
@@ -56,7 +56,7 @@
 			<div class="btn-row">
 				<a href="<?= site_url('/aeromodel/');?>" class="btn btn-default">Aeromodel</a> 
 				<a href="<?= site_url('/craft/');?>" class="btn btn-default">Craft</a> 
-				<a href="<?= site_url('/stage-set-building-film-theatre/');?>" class="btn btn-default">Stage, Set & Film</a> 
+				<a href="<?= site_url('/education-primary-secondary-teaching/');?>" class="btn btn-default">Education</a> 
 				
 
 				<div class="dropdown dropdown-inline">
@@ -65,12 +65,9 @@
 				    <span class="caret"></span>
 				  </button>
 				  <ul class="dropdown-menu" aria-labelledby="dLabel">
-				    <li><a href="">Education - Students</a></li>
-				    <li><a href="">Education - Teachers</a></li>
-				    <li><a href="">Promotional Products</a></li>
-				    <li><a href="">Architecture Models</a></li>
-				    <li><a href="">Surfboards & SUPs</a></li>
-				    <li><a href="">Fibreglassing & Industrial</a></li>
+				    <li><a href="<?= site_url('/stage-set-building-film-theatre/');?>">Stage, Set &amp; Film</a></li>
+				    <li><a href="<?= site_url('/promotional-products/');?>">Promotional Products</a></li>
+				    <li><a href="<?= site_url('/surfboard-balsa-wood/');?>">Surfboards &amp; SUPs</a></li>
 				  </ul>
 				</div>
 			</div>
@@ -90,9 +87,9 @@
 
 		<div class="block-grid">
 	<div class="container">
-		<div class="row wow fadeIn">
+		<div class="row">
 			<div class="grid-image grid-left col-sm-6 grid-match">
-				<a href="">
+				<a href="<?= site_url('/shop/');?>">
 					<img src="<?php bloginfo('template_directory'); ?>/dist/images/mixed-balsa-wood-shapes.jpg"  alt="A selection of Balsa Wood Shapes including Dowels, Sticks and Blocks">
 				</a>
 			</div>
@@ -107,7 +104,7 @@
 		</div><!-- /.row -->
 		<div class="row wow fadeIn">
 			<div class="grid-image grid-right col-sm-6 col-sm-push-6 grid-match">
-				<a href="">
+				<a href="<?= site_url('/other-ways-to-order/');?>">
 					<img src="<?php bloginfo('template_directory'); ?>/dist/images/storefront-1.jpg" alt="The balsa central storefornt is well stocked">
 				</a>
 			</div>
@@ -116,7 +113,7 @@
 					<div class="grid-link">
 					<h3 class="headline">We're local too</h3>
 					<p class="lead">Balsa Central is located in Melrose Park, South Australia. We welcome walk in business, phone orders etc. Talk to real people!</p>
-					<p><a href="" class="btn btn-primary">Our Location</a> <a href="<?= site_url('/other-ways-to-order/');?>" class="btn btn-primary">Offline Orders</a></p>
+					<p><a href="<?= site_url('/contact-us/');?>" class="btn btn-primary">Our Location</a> <a href="<?= site_url('/other-ways-to-order/');?>" class="btn btn-primary">Offline Orders</a></p>
 				</div>
 				</div>
 			</div>
@@ -145,8 +142,8 @@
 	<section class="page-section featured-products wow fadeIn">
 		<div class="container">
 			<div class="row">
-				<h3 class="headline">Our best sellers</h3>
-				<?php echo do_shortcode('[featured_products per_page="4" columns="4"]'); ?>
+				<h2 class="headline text-center">Our best sellers</h2>
+				<?php echo do_shortcode('[featured_products per_page="8" columns="4"]'); ?>
 			</div>
 		</div><!-- /.container -->
 	</section>
